ebeda0b129adde7a837a7c9357d33a6aed14b7cb
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2025-07-19 19:00:44 +0100

    Update example.lua

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -9,9 +9,14 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
+  {
+  'stevedylandev/darkmatter-nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd('colorscheme darkmatter')
+  end,
+},
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
