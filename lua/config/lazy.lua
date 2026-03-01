5e231b03839b59be12a39ca0505679c9c43aa5f5
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2026-03-01 16:56:17 +0000

    refactor original example.lua to function as a valid plugins file without errors, rename to plugins.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,8 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- add LazyVim extras
+    { import = "lazyvim.plugins.extras" },
     -- import/override with your plugins
     { import = "plugins" },
   },
